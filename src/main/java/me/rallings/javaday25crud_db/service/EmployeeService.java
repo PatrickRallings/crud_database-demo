0f6cd71f20fad905a64955989c006cd711f314f3
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-06 10:36:17 -0400

    debug - removed service anot

--- src/main/java/me/rallings/javaday25crud_db/service/EmployeeService.java
+++ src/main/java/me/rallings/javaday25crud_db/service/EmployeeService.java
@@ -1,11 +1,9 @@
 package me.rallings.javaday25crud_db.service;
 
 import me.rallings.javaday25crud_db.model.Employee;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface EmployeeService {
     List<Employee> getAllEmployees();
 }
--- src/main/java/me/rallings/javaday25crud_db/service/EmployeeServiceImpl.java
+++ src/main/java/me/rallings/javaday25crud_db/service/EmployeeServiceImpl.java
@@ -3,9 +3,11 @@ package me.rallings.javaday25crud_db.service;
 import me.rallings.javaday25crud_db.model.Employee;
 import me.rallings.javaday25crud_db.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
