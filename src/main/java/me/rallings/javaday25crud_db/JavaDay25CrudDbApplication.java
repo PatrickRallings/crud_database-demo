c38980680810cc8d3f75b5013eb2c295899c51b5
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-06 08:42:08 -0400

    Initial commit

---
+++ src/main/java/me/rallings/javaday25crud_db/JavaDay25CrudDbApplication.java
@@ -0,0 +1,13 @@
+package me.rallings.javaday25crud_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDay25CrudDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDay25CrudDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rallings/javaday25crud_db/JavaDay25CrudDbApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.javaday25crud_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDay25CrudDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
